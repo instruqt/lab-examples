f9e6fc8ab1279171445d9c26b1ffefdd0b0b0fa8
Erik Veld <erik.veld@gmail.com>
2025-05-15 16:29:14 +0200

    disable host checking

--- google-project/scripts/file_exists.sh
+++ google-project/scripts/file_exists.sh
@@ -9,4 +9,4 @@ apk add -u jq
 
 IP=$(gcloud compute instances describe my-instance --zone europe-west1-b --format=json | jq -r .networkInterfaces[0].accessConfigs[0].natIP)
 
-ssh -i {{key}} {{user}}@${IP} 'test -f /tmp/hello'
+ssh -i {{key}} {{user}}@${IP} -o StrictHostKeyChecking=no 'test -f /tmp/hello'
