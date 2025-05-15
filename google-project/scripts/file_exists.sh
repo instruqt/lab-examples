403a523800f613cddc36cf497fb2610cab47eb1c
Erik Veld <erik.veld@gmail.com>
2025-05-15 16:56:57 +0200

    double escape?

--- google-project/scripts/file_exists.sh
+++ google-project/scripts/file_exists.sh
@@ -6,7 +6,7 @@
 
 # Make sure jq is there
 cat <<EOF > /tmp/test.sh
-ssh -i ${key} -o StrictHostKeyChecking=no ${user}@${host} 'test -f /tmp/hello'
+ssh -i $${key} -o StrictHostKeyChecking=no $${user}@$${host} 'test -f /tmp/hello'
 EOF 
 
-ssh -i ${key} -o StrictHostKeyChecking=no ${user}@${host} 'test -f /tmp/hello'
+ssh -i $${key} -o StrictHostKeyChecking=no $${user}@$${host} 'test -f /tmp/hello'
