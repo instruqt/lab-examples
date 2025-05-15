630f2338f8d70e12f076892b18de5a26a4dc548f
Erik Veld <erik.veld@gmail.com>
2025-05-15 19:39:43 +0200

    try with simple env vars

--- google-project/scripts/file_exists.sh
+++ google-project/scripts/file_exists.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
-# gcloud compute ssh {{user}}@{{host}} \
-#   --zone={{zone}} --force-key-file-overwrite \
-#   --ssh-key-file={{key}} \
-#   --command "test -f /tmp/hello"
-
-# Make sure jq is there
 cat <<EOF > /tmp/test.sh
-ssh -i $${key} -o StrictHostKeyChecking=no $${user}@$${host} 'test -f /tmp/hello'
+ssh -i $key -o StrictHostKeyChecking=no $user@$host 'test -f /tmp/hello'
 EOF 
 
-ssh -i $${key} -o StrictHostKeyChecking=no $${user}@$${host} 'test -f /tmp/hello'
+ssh -i $key -o StrictHostKeyChecking=no $user@$host 'test -f /tmp/hello'
