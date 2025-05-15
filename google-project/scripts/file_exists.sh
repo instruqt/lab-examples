e088568479c0fbc742f3c0b9e7b65cc2c40fe47c
Erik Veld <erik.veld@gmail.com>
2025-05-15 16:22:39 +0200

    work around gcloud not running command

--- google-project/scripts/file_exists.sh
+++ google-project/scripts/file_exists.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-gcloud compute ssh {{user}}@{{host}} \
-  --zone={{zone}} --force-key-file-overwrite \
-  --ssh-key-file={{key}} \
-  --command "test -f /tmp/hello"
+# gcloud compute ssh {{user}}@{{host}} \
+#   --zone={{zone}} --force-key-file-overwrite \
+#   --ssh-key-file={{key}} \
+#   --command "test -f /tmp/hello"
+
+# Make sure jq is there
+apk add -u jq
+
+IP=$(gcloud compute instances describe my-instance --zone europe-west1-b --format=json | jq -r .networkInterfaces[0].accessConfigs[0].natIP)
+
+ssh -i {{key}} {{user}}@${IP} 'test -f /tmp/hello'
