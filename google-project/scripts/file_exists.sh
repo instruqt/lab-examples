d5e4285d102c0aa54ce639854c726fdb10c6d47c
Erik Veld <erik.veld@gmail.com>
2025-05-16 21:20:59 +0200

    restore check

--- google-project/scripts/file_exists.sh
+++ google-project/scripts/file_exists.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-# ssh -i "$key" -o StrictHostKeyChecking=no "$user@$host" 'test -f /tmp/hello'
-
-echo "bla"
-exit 214
+ssh -i "$key" -o StrictHostKeyChecking=no "$user@$host" 'test -f /tmp/hello'
