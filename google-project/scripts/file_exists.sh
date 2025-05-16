eb422f233c0684335eed532335459b13174dde93
Erik Veld <erik.veld@gmail.com>
2025-05-16 19:29:18 +0200

    break on purpose for test

--- google-project/scripts/file_exists.sh
+++ google-project/scripts/file_exists.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-ssh -i "$key" -o StrictHostKeyChecking=no "$user@$host" 'test -f /tmp/hello'
+# ssh -i "$key" -o StrictHostKeyChecking=no "$user@$host" 'test -f /tmp/hello'
+cat <<EOF > test.sh
+echo "break on purpose"
+EOF 
+
+echo "bla"
