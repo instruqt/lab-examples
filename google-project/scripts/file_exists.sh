930d4fb471a4956779cdc7b84f9a0235fbf4532e
Erik Veld <erik.veld@gmail.com>
2025-05-16 13:29:08 +0200

    cleanup

--- google-project/scripts/file_exists.sh
+++ google-project/scripts/file_exists.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-cat <<-EOF > /tmp/test.sh
-ssh -i "$key" -o StrictHostKeyChecking=no "$user@$host" 'test -f /tmp/hello'
-EOF
-
 ssh -i "$key" -o StrictHostKeyChecking=no "$user@$host" 'test -f /tmp/hello'
